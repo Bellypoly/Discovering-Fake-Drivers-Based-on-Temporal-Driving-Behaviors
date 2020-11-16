68a48f7b19448bd698a67d0c8ffb401decd9debd
Hashim Abu-gellban <gellban@gmail.com>
2020-11-15 20:38:47 -0600

    update repository - Hashim

--- preprocessing.r
+++ preprocessing.r
@@ -3,6 +3,13 @@ getwd()
 #file_org_dataset <- "./dataset/Driving_Data_KIA_SOUL.csv"
 #"D:/Ingenio/Dataset/training/facetraining.csv"
 
+min_max_normalization <- function(data_df_X){
+  X<- scale(data_df_X)
+  #X <- data_df[,!(names(data_df) %in% target_column)]
+  X <- as.data.frame(apply(X[, 1:ncol(X)-1], 2, function(x) (x - min(x))/(max(x)-min(x))))
+  #data_df[,!(names(data_df) %in% target_column)] <- X
+  return (X)
+}
 get_dataset_org <- function(file_org_dataset){
   data_df_org <- read.csv(file_org_dataset)
 #  str(data_df_org)
@@ -19,8 +26,16 @@ get_dataset_org <- function(file_org_dataset){
   #head(data_df[,!(names(data_df) %in% target_column)])
   #sum(is.na(scale(data_df[1:10,!(names(data_df) %in% target_column)])))
   
-  #scale only X without y
-  data_df[,!(names(data_df) %in% target_column)]<- scale(data_df[,!(names(data_df) %in% target_column)])
+#  #scale only X without y
+#  #data_df[,!(names(data_df) %in% target_column)]<- scale(data_df[,!(names(data_df) %in% target_column)], scale = FALSE)
+#  data_df[,!(names(data_df) %in% target_column)]<- scale(data_df[,!(names(data_df) %in% target_column)])
+#  #data_df_scaled <- scale(data_df[,!(names(data_df) %in% target_column)])
+#  #data_df[,!(names(data_df) %in% target_column)] <-  apply(data_df_scaled, 2, sd)
+#  X <- data_df[,!(names(data_df) %in% target_column)]
+#  X <- as.data.frame(apply(X[, 1:ncol(X)-1], 2, function(x) (x - min(x))/(max(x)-min(x))))
+#  data_df[,!(names(data_df) %in% target_column)] <- X
+  data_df[,!(names(data_df) %in% target_column)] <- min_max_normalization(data_df[,!(names(data_df) %in% target_column)])
+  
   
   
   #x[1:2,]
@@ -36,36 +51,39 @@ get_dataset_org <- function(file_org_dataset){
 
 data_df_scaled <- get_dataset_org(file_org_dataset)
 dim(data_df_scaled)
-#data_df_org <- read.csv(file_org_dataset)
+data_df_scaled[, selected_signals]
+colnames(data_df_scaled)
+#as.data.frame(apply(data_df_scaled[, 1:ncol(data_df_scaled)-1], 2, function(x) (x - min(x))/(max(x)-min(x))))
+# check that we get mean of 0 and sd of 1
+colMeans(data_df_scaled)  # faster version of apply(scaled.dat, 2, mean)
+#apply(data_df_scaled, 2, sd)
 
-#dataset description: https://www.iit.cnr.it/sites/default/files/human-behavior-characterization.pdf
+#http://www.sthda.com/english/wiki/one-way-anova-test-in-r
+#Anova
+aov(Class ~ ., data = data_df_scaled)
 
-str(data_df_org)
-unique(data_df_org$PathOrder)
-data_df <- subset(data_df_org, PathOrder==1)
-drop_columns <- c("Time", "PathOrder")
-target_column <- c("Class")
-head(data_df)
-data_df <- data_df[,!(names(data_df) %in% drop_columns)]
+#library(preprocessCore)
+#normalize(data_df_scaled)
+#normalize(x, method = "standardize", range = c(0, 1), margin = 1L, on.constant = "quiet")
+data_df_scaled[,]
+chisq <- chisq.test(data_df_scaled[,!(names(data_df_scaled) %in% target_column)])
+# Observed counts
+chisq$observed
+round(chisq$expected,2)
+round(chisq$residuals, 3)
+library(corrplot)
+corrplot(chisq$residuals, is.cor = FALSE)
 
-#data_df$Class <- factor(data_df$Class)                  
-#data_df$Class
-#unique(as.numeric(factor(data_df$Class)))
+disc<-"equal interval width"
+X <- data_df_scaled[,!(names(data_df_scaled) %in% target_column)]
+select.inf.chi2(X,disc.method=disc,attrs.nominal=attrs.nominal)
 
-#replace letters with numbers
-data_df$Class <- as.numeric(factor(data_df$Class))  
-unique(data_df$Class)   
-#head(data_df[,!(names(data_df) %in% target_column)])
-sum(is.na(scale(data_df[1:10,!(names(data_df) %in% target_column)])))
 
-#scale only X without y
-data_df[,!(names(data_df) %in% target_column)]<- scale(data_df[,!(names(data_df) %in% target_column)])
-#x[1:2,]
-#zero_columns <- c("Filtered_Accelerator_Pedal_value", "Inhibition_of_engine_fuel_cut_off", "Fuel_Pressure", "")
-#colnames(x)[colSums(is.na(x)) > 0]
-
-#Drop Zero columns: Some columns have zero values which are useless. They result in null values after scaling data. We drop them
-zero_columns <- colnames(data_df)[colSums(is.na(data_df)) > 0]
-data_df <- data_df[,!(names(data_df) %in% zero_columns)]
-head(data_df)
-dim(data_df)
+# prepare training scheme
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(Class~., data=data_df_scaled, method="lvq", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
--- setup.r
+++ setup.r
@@ -2,3 +2,20 @@ getwd() #"/Users/hgellban
 #/Users/hgellban/Documents/111_one_drive/OneDrive - Texas Tech University/ttu-cs/2020fall/programming_with_R/project/code
 setwd("/Users/hgellban/Documents/111_one_drive/OneDrive - Texas Tech University/ttu-cs/2020fall/programming_with_R/project/code/Discovering-Fake-Drivers-Based-on-Temporal-Driving-Behaviors")
 file_org_dataset <- "./dataset/Driving_Data_KIA_SOUL.csv"
+#dataset description: https://www.iit.cnr.it/sites/default/files/human-behavior-characterization.pdf
+
+
+selected_signals <- c(
+  "Accelerator_Pedal_value",
+  "Short_Term_Fuel_Trim_Bank1",
+  "Intake_air_pressure",
+  #"Filtered_Accelerator_Pedal_value",
+  "Engine_soacking_time",
+  "Acceleration_speed_._Longitudinal",
+  "Indication_of_brake_switch_ON.OFF",
+  "Master_cylinder_pressure",
+  "Calculated_road_gradient",
+  "Acceleration_speed_._Lateral",
+  "Steering_wheel_speed",
+  "Steering_wheel_angle"
+)
