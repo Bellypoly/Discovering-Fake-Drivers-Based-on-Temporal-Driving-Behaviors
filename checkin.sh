bd96d44e38c511c43cd54b62af44a384ebf0ef4a
Hashim Abu-gellban <gellban@gmail.com>
2020-11-17 22:18:31 -0600

    update repository - Hashim

--- checkin.sh
+++ checkin.sh
@@ -1,3 +1,4 @@
+#git pull https://github.com/gellban/Discovering-Fake-Drivers-Based-on-Temporal-Driving-Behaviors.git master:dev
 git add .
 git commit -m "update repository - Hashim"
 git push -u origin master
